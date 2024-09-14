595985b996140ed398a750ae58dc986748bd31c2
Poorna Kamat <kamatpoorna@gmail.com>
2024-09-15 00:15:08 +0530

    feat: Header=> Added alerk to manage authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -19,8 +21,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={cn("min-h-screen font-sans antialiased", fontSans.variable)}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#715BEB",
+          fontSize: "16px",
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn("min-h-screen font-sans antialiased", fontSans.variable)}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
