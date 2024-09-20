cb525dbbfd1eb9c5aafc9e154f536ebace5c91a3
Poorna Kamat <kamatpoorna@gmail.com>
2024-09-20 13:27:20 +0530

    feat: Added Sentry

---
+++ sentry.client.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c646efc79ed596546902120f41bdcd06@o4507978783588352.ingest.de.sentry.io/4507978790731856",
+
+  integrations: [Sentry.replayIntegration()],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
